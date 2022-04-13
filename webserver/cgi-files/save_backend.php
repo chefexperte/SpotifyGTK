d724992e5d4102b6f3980c5784a22f8400063f3e
Chef Experte <aago2001@web.de>
2022-04-13 16:41:11 +0200

    Implemented loading token from file Implemented refreshing token with refresh_token Fully implemented getting code and token with refresh_token Moved clientId and clientSecret to extra file (so I don't accidentally upload it)

--- webserver/cgi-files/save_backend.php
+++ webserver/cgi-files/save_backend.php
@@ -5,11 +5,12 @@ if(!empty($_POST['token'])){
 $token = $_POST['token'];
 $expire = $_POST['expiration'];
 $refresh = $_POST['refresh_token'];
-$file = fopen("../token.txt", 'w');//creates new file
-fwrite($file, $token . "\n" . $expire . "\n" . $refresh);
+$file = fopen("../token.txt", 'w');
+$expire_secs = time() + (int) $expire;
+fwrite($file, $token . "\n" . $expire_secs . "\n" . $refresh);
 fclose($file);
 }else{
-$file = fopen("../no_data_given.txt", 'w');//creates new file
+$file = fopen("../no_data_given.txt", 'w');
 fwrite($file, "\$_POST data: ");
 fwrite($file, file_get_contents('php://input'));
 foreach($_POST as $item) {
--- webserver/cgi-files/script.js
+++ webserver/cgi-files/script.js
@@ -1,4 +1,7 @@
+#!/usr/bin/php-cgi
+
 // Get the hash of the url
+console.log(window.location.hash);
 const hash = window.location.hash
     .substring(1)
     .split('&')
@@ -9,7 +12,7 @@ const hash = window.location.hash
         }
         return initial;
     }, {});
-
+console.log(location.search);
 const get = location.search
     .substring(1)
     .split('&')
@@ -25,20 +28,49 @@ window.location.hash = '';
 // Set token
 console.log(get.error)
 let _error = get.error;
+let _code = get.code;
 let _token = hash.access_token;
 let _expire = hash.expires_in;
 let _refresh = hash.refresh_token;
+let request_refresh = false;
 
-const authEndpoint = 'https://accounts.spotify.com/authorize';
+const authEndpoint = 'https://accounts.spotify.com';
 
 // Replace with your app's client ID, redirect URI and desired scopes
-const clientId = 'PUT IT HERE';
+<?php
+if (file_exists("../client-auth.txt")) {
+    $file = file("../client-auth.txt");
+    $clientId  = substr($file[0], 0, -1);
+    $clientSecret  = substr($file[1], 0, -1);
+    echo "const clientId = '" . $clientId . "';\n";
+    echo "const clientSecret = '" . $clientSecret . "';\n";
+}
+?>
 const redirectUri = 'http://localhost:8080/index.html';
 const scopes = [
     'streaming',
     'user-read-email',
     'user-read-private'
 ];
+
+<?php
+echo "time_now = " . time() . ";\n";
+if (file_exists("../token.txt")) {
+    $file = file("../token.txt");
+    $time_now = time();
+    $expire_time = (int) substr($file[1], 0, -1);
+    $refresh_token = substr($file[2], 0, -1);
+    $token = substr($file[0], 0, -1);
+    if ($expire_time > $time_now) {
+        echo "_token = \"" . $token . "\";\n";
+        echo "_expire = \"" . $expire_time . "\";\n";
+        echo "_refresh = \"" . $refresh_token . "\";\n";
+    } else {
+        echo "request_refresh = true;";
+    }
+    //echo $expire_time . "-" . $time_now . " = " . $expire_time-$time_now;
+}
+?>
 
 function saveToken(token, expiration, refresh_token) {
     var data = new FormData();
@@ -50,26 +82,59 @@ function saveToken(token, expiration, refresh_token) {
     xhr.send(data);
 }
 
-// If there is no token, redirect to Spotify authorization
-if (!_token && !_error) {
-    window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
-} else {
-    if (!_error) {
+if (_error) {
+    msg = window.document.getElementById("initMessage");
+    msg.style.color = "red";
+    msg.textContent = _error;
+    try_again = document.createElement("button");
+    try_again.innerText = "Try again"
+    try_again.onclick = function () {
+        window.location = "/"
+    }
+    msg.parentNode.insertBefore(try_again, msg.nextSibling);
+    console.log("Error received.");
+} else if (request_refresh) {
+    var b64 = btoa(`${clientId}:${clientSecret}`);
+    fetch(`${authEndpoint}/api/token`, {
+        method: 'POST',
+        body: new URLSearchParams({
+            'grant_type': 'refresh_token',
+            'refresh_token': _refresh
+        }),
+        headers: {
+            'Authorization': `Basic ${b64}`
+        }
+    }).then(res => res.json()).then(data => {
+        _token = data.access_token;
+        _expire = time_now + 3600;
         saveToken(_token, _expire, _refresh);
         console.log("Token saved.");
-    } else {
-        msg = window.document.getElementById("initMessage");
-        msg.style.color = "red";
-        msg.textContent = _error;
-        try_again = document.createElement("button");
-        try_again.innerText = "Try again"
-        try_again.onclick = function () {
-            window.location = "/"
+    });
+} else if (_code) {
+    // We have no error, but a code. So request a token now.
+    var b64 = btoa(`${clientId}:${clientSecret}`);
+    fetch(`${authEndpoint}/api/token`, {
+        method: 'POST',
+        body: new URLSearchParams({
+            'grant_type': 'authorization_code',
+            'code': _code,
+            'redirect_uri': redirectUri
+        }),
+        headers: {
+            'Authorization': `Basic ${b64}`
         }
-        msg.parentNode.insertBefore(try_again, msg.nextSibling);
-        console.log("Error received.");
-    }
+    }).then(res => res.json()).then(data => {
+        _token = data.access_token;
+        _expire = data.expires_in;
+        _refresh = data.refresh_token;
+        saveToken(_token, _expire, _refresh);
+        console.log("Token saved.");
+    });
+    history.replaceState(null, "", location.href.split("?")[0]);
+} else if (_token) {
 
+} else {
+    window.location = `${authEndpoint}/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=code&show_dialog=false`;
 }
 
 window.onSpotifyWebPlaybackSDKReady = () => {
