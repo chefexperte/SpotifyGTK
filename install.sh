c510997d94bd0c9feb2fafaa6c855c2801f80b67
Chef Experte <aago2001@web.de>
2022-04-17 01:35:29 +0200

    Added install.sh as installer script for downloading geckodriver and installing pip packages for easy running

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Download geckodriver
+echo "Downloading geckodriver..."
+type=linux64
+url=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | jq -r ".assets[] | select(.name | test(\"${type}\")) | .browser_download_url" | head -n 1)
+curl -L -o geckodriver.tar.gz "$url"
+tar -xzf geckodriver.tar.gz
+rm geckodriver.tar.gz
+chmod +x geckodriver
+
+# Install pip packages
+echo "Installing pip packages"
+pip install selenium
+pip install pygobject
