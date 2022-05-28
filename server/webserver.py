2437ae0fdb23b24ce3b404c7797f6e5e761f0fcc
Chef Experte <aago2001@web.de>
2022-05-28 03:10:02 +0200

    Updated formatting

--- server/webserver.py
+++ server/webserver.py
@@ -18,35 +18,35 @@ class MyServer(handler):
 
     cgi_list: [str] = None
 
-    def get_post_data(self) -> dict[str, str]:
-        """
-        Formats the POST data from the header and returns it
-        :return: a dictionary with the POST variables and values
-        """
-        form = cgi.FieldStorage(fp=self.rfile, headers=self.headers,
-                                environ={'REQUEST_METHOD': 'POST', 'CONTENT_TYPE': self.headers['Content-Type'], })
-        data = dict()
-        for item in form.list:
-            data[item.name] = item.value
-        return data
+	def get_post_data(self) -> dict[str, str]:
+		"""
+		Formats the POST data from the header and returns it
+		:return: a dictionary with the POST variables and values
+		"""
+		form = cgi.FieldStorage(fp=self.rfile, headers=self.headers,
+		                        environ={'REQUEST_METHOD': 'POST', 'CONTENT_TYPE': self.headers['Content-Type'], })
+		data = dict()
+		for item in form.list:
+			data[item.name] = item.value
+		return data
 
-    def clean_path(self):
-        # Removes the first '/', GET variables and anchors from the path, leaving a clean path and filename
-        return self.path[1:].split("?")[0].split("#")[0]
+	def clean_path(self):
+		# Removes the first '/', GET variables and anchors from the path, leaving a clean path and filename
+		return self.path[1:].split("?")[0].split("#")[0]
 
-    def do_GET(self):
-        if self.clean_path() in self.cgi_list:
-            self.path = "/cgi-files" + self.path
-        else:
-            self.path = "/webapp" + self.path
-        CGIHTTPRequestHandler.do_GET(self)
+	def do_GET(self):
+		if self.clean_path() in self.cgi_list:
+			self.path = "/cgi-files" + self.path
+		else:
+			self.path = "/webapp" + self.path
+		CGIHTTPRequestHandler.do_GET(self)
 
-        # self.send_response(200)
-        # self.send_header("Content-type", "text/html")
-        # self.end_headers()
-        # valid_files = ["index.html", "script.js", "save_backend.php", "test.php"]
-        # if file in valid_files:
-        #     self.wfile.write(open("webapp/" + file, "rb").read())
+		# self.send_response(200)
+		# self.send_header("Content-type", "text/html")
+		# self.end_headers()
+		# valid_files = ["index.html", "script.js", "save_backend.php", "test.php"]
+		# if file in valid_files:
+		#     self.wfile.write(open("webapp/" + file, "rb").read())
 
     def do_POST(self):
         if self.clean_path() in self.cgi_list:
@@ -58,16 +58,16 @@ class MyServer(handler):
 
 
 def prepare_php_ini():
-    open("php.ini", "w").write("cgi.force_redirect = 0\ndoc_root = " + getcwd())
+	open("php.ini", "w").write("cgi.force_redirect = 0\ndoc_root = " + getcwd())
 
 
 def prepare_cgi_list():
-    # list with files that have to be executed as a cgi, and thus are placed in the cgi folder
-    MyServer.cgi_list = []
-    for file in os.listdir("cgi-files"):
-        MyServer.cgi_list.append(file)
-    print("CGI scripts: ")
-    [print(name) for name in MyServer.cgi_list]
+	# list with files that have to be executed as a cgi, and thus are placed in the cgi folder
+	MyServer.cgi_list = []
+	for file in os.listdir("cgi-files"):
+		MyServer.cgi_list.append(file)
+	print("CGI scripts: ")
+	[print(name) for name in MyServer.cgi_list]
 
 
 class Webserver:
@@ -84,16 +84,16 @@ class Webserver:
     def run(self):
         self.web_server.serve_forever()
 
-    def stop(self):
-        self.web_server.server_close()
+	def stop(self):
+		self.web_server.server_close()
 
 
 if __name__ == "__main__":
-    webserver = Webserver()
-    try:
-        webserver.run()
-    except KeyboardInterrupt:
-        pass
+	webserver = Webserver()
+	try:
+		webserver.run(None)
+	except KeyboardInterrupt:
+		pass
 
-    webserver.stop()
-    print("Server stopped.")
+	webserver.stop()
+	print("Server stopped.")
